b5b84302aa60701388523dfc3df98343da42252b
huchangzhou.3 <huchangzhou.3@jd.com>
2025-04-30 16:46:30 +0800

    main1.0

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("This is a simple Go program.")
+	fmt.Println("It prints a message to the console.")
+}
