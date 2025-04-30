9a5d00d773728ec005be8487c4f73e9d70046bd8
huchangzhou.3 <huchangzhou.3@jd.com>
2025-04-30 16:48:11 +0800

    main2.0

--- main.go
+++ main.go
@@ -8,4 +8,5 @@ func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("This is a simple Go program.")
 	fmt.Println("It prints a message to the console.")
+	fmt.Println("You can modify this code to suit your needs.")
 }
